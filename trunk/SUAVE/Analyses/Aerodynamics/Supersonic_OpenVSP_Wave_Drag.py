6fd5fa2c73f85e2af052153050d7949269d05860
Tim MacDonald <timdmacdo@gmail.com>
2017-06-21 17:08:13 -0700

    comment changes

--- trunk/SUAVE/Analyses/Aerodynamics/Supersonic_OpenVSP_Wave_Drag.py
+++ trunk/SUAVE/Analyses/Aerodynamics/Supersonic_OpenVSP_Wave_Drag.py
@@ -1,9 +1,7 @@
 # Supersonic_Zero.py
 # 
 # Created:            T. MacDonald
-# Modified: Nov 2016, T. MacDonald
-#
-# Based on Fidelity_Zero
+# Modified: Apr 2017, T. MacDonald
 
 
 # ----------------------------------------------------------------------
@@ -38,7 +36,7 @@ class Supersonic_OpenVSP_Wave_Drag(Markup):
     
     def __defaults__(self):
         
-        self.tag = 'Fidelity_Zero_Supersonic'
+        self.tag = 'OpenVSP_Supersonic'
         
         # correction factors
         settings =  self.settings
@@ -64,36 +62,39 @@ class Supersonic_OpenVSP_Wave_Drag(Markup):
         
         compute.lift = Process()
         compute.lift.inviscid_wings                = Vortex_Lattice()
-        compute.lift.vortex                        = Methods.Lift.vortex_lift  # SZ
-        compute.lift.compressible_wings            = Methods.Lift.wing_compressibility # SZ
-        compute.lift.fuselage                      = Methods.Lift.fuselage_correction # difference in results storage
-        compute.lift.total                         = Methods.Lift.aircraft_total # no difference
+        compute.lift.vortex                        = Methods.Lift.vortex_lift
+        compute.lift.compressible_wings            = Methods.Lift.wing_compressibility
+        compute.lift.fuselage                      = Methods.Lift.fuselage_correction
+        compute.lift.total                         = Methods.Lift.aircraft_total
         
         compute.drag = Process()
         compute.drag.compressibility               = Process()
-        compute.drag.compressibility.total         = VSP_Methods.compressibility_drag_total # SZ        
+        compute.drag.compressibility.total         = VSP_Methods.compressibility_drag_total       
         compute.drag.parasite                      = Process()
         compute.drag.parasite.wings                = Process_Geometry('wings')
-        compute.drag.parasite.wings.wing           = Methods.Drag.parasite_drag_wing # SZ
+        compute.drag.parasite.wings.wing           = Methods.Drag.parasite_drag_wing
         compute.drag.parasite.fuselages            = Process_Geometry('fuselages')
-        compute.drag.parasite.fuselages.fuselage   = Methods.Drag.parasite_drag_fuselage # SZ
+        compute.drag.parasite.fuselages.fuselage   = Methods.Drag.parasite_drag_fuselage
         compute.drag.parasite.propulsors           = Process_Geometry('propulsors')
-        compute.drag.parasite.propulsors.propulsor = Methods.Drag.parasite_drag_propulsor # SZ
-        #compute.drag.parasite.pylons               = Methods.Drag.parasite_drag_pylon
-        compute.drag.parasite.total                = Methods.Drag.parasite_total # SZ
-        compute.drag.induced                       = Methods.Drag.induced_drag_aircraft # SZ
-        compute.drag.miscellaneous                 = Methods.Drag.miscellaneous_drag_aircraft # different type used in FZ
-        compute.drag.untrimmed                     = Methods.Drag.untrimmed # SZ can be changed to match
-        compute.drag.trim                          = Methods.Drag.trim # SZ can be chanaged to match
-        compute.drag.total                         = Methods.Drag.total_aircraft # SZ
+        compute.drag.parasite.propulsors.propulsor = Methods.Drag.parasite_drag_propulsor
+        #compute.drag.parasite.pylons               = Methods.Drag.parasite_drag_pylon # supersonic pylon methods not currently available
+        compute.drag.parasite.total                = Methods.Drag.parasite_total
+        compute.drag.induced                       = Methods.Drag.induced_drag_aircraft
+        compute.drag.miscellaneous                 = Methods.Drag.miscellaneous_drag_aircraft
+        compute.drag.untrimmed                     = Methods.Drag.untrimmed 
+        compute.drag.trim                          = Methods.Drag.trim
+        compute.drag.total                         = Methods.Drag.total_aircraft
         
         
     def initialize(self):
         import os
+        
+        # Remove old volume drag data so that new data can be appended without issues
         try:
             os.remove('volume_drag_data.npy')  
         except:
             pass
+        
         self.process.compute.lift.inviscid_wings.geometry = self.geometry
         self.process.compute.lift.inviscid_wings.initialize()
         
