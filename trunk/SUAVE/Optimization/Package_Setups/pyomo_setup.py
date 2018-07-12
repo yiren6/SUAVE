81b7cebf89a97cabaa8bbb7714c0f9470ef65bc0
jmvegh <jmvegh@stanford.edu>
2018-07-12 11:29:02 -0700

    now have exit code 3 on baron

--- trunk/SUAVE/Optimization/Package_Setups/pyomo_setup.py
+++ trunk/SUAVE/Optimization/Package_Setups/pyomo_setup.py
@@ -83,7 +83,10 @@ def Pyomo_Solve(problem, solver = 'baron'):
     #for j in range(len(constraints[:,0])):
 
     opt      = pyo.SolverFactory(solver)
-    results = opt.solve(pyomo_problem)
+    print 'solver = ', solver
+    print 'opt = ', opt
+    #results = opt.solve(pyomo_problem)
+    results = opt.solve(pyomo_problem.model, tee=True)
     #instance.display()
     
     return result
@@ -94,6 +97,7 @@ import pyomo.environ as pyo
     #instantiate a pyomo problem that can be solved
     
 class Pyomo_Problem(RealOptProblem):
+    #note; pyomo runs through problem at the initial values before 
     def __init__(self, problem):
         RealOptProblem.__init__(self)
         self.problem  = problem
@@ -101,10 +105,7 @@ class Pyomo_Problem(RealOptProblem):
         self.grad_f   = None
         self.jac_g    = None
         self.x_grad   = [-1E7] #save value for x that gradient was computed so you don't call SUAVE twice when computing gradient and jacobian of constraints
-        self.response_types = [response_enum.FunctionValue, 
-                            response_enum.Gradient,
-                            response_enum.NonlinearConstraintValues,
-                            response_enum.Jacobian]
+        #self.response_types = [response_enum.FunctionValue,  response_enum.Gradient, response_enum.NonlinearConstraintValues, response_enum.Jacobian]
         
         
         #Pull out the basic problem
@@ -136,28 +137,37 @@ class Pyomo_Problem(RealOptProblem):
         model.ncon    = ncon
       
         #model.x       = pyo.Var(pyo.RangeSet(model.nvars), initialize = ini)#pyo.Var()
-        model.x       = pyo.Var(pyo.RangeSet(model.nvars))#pyo.Var()
+        model.x       = pyo.Var(pyo.RangeSet(model.nvars), domain= pyo.Reals) #pyo.Var(pyo.RangeSet(model.nvars), bounds = (flbd, fubd), initialize = x0[0])#pyo.Var() #pyo.Var(pyo.RangeSet(model.nvars))
+        
         
         for ii in xrange(0,nvar):
             flbd[ii]      = (bnd[ii][0]/scl[ii])
             fubd[ii]      = (bnd[ii][1]/scl[ii])
             model.x[ii+1] = x0[ii] #assign values
-            #model.x[ii+1] = pyo.Var( bounds=(flbd[ii], fubd[ii]), initialize=ini[ii])
-        print 'x here = ', model.x
-        print 'dir(x) = ', dir(model.x)
-        print 'x[values] = ', model.x.get_values().values()
+            model.x[ii+1].setlb(flbd[ii])
+            model.x[ii+1].setub(fubd[ii])
+            #model.x[ii+1] = pyo.Var( bounds=(flbd[ii], fubd[ii]), initialize=x0[ii])
+        
+            
+            
+   
         #model.lower   = flbd
         #model.upper   = fubd
         self.lower     = flbd
         self.higher    = fubd
-        print 'self.lower = ', self.lower
-        print 'self.higher = ', self.higher
+        
         #model.x          = pyo.Var(pyo.RangeSet(model.nvars), within=pyo.Reals, initialize = ini)
+    
         model.constraint = pyo.Constraint(pyo.RangeSet(model.ncon), rule = self.constraint_rule)
         model.problem    = problem  #assign problem to model
         self.model       = model
-        
-        
+        model.objective  = pyo.Objective(rule = self.objective_rule, sense = pyo.minimize)
+        #model.lower      = self.lower
+        #model.upper      = self.upper
+        #expose these functions to top level solver
+        #self.valid_problem_types = model.valid_problem_types #kind of hacky
+        #self.write               = model.write
+        print 'got to end of init'
         
         
         #now use the SUAVE problem to create a pyomo model that can be solved
@@ -166,17 +176,18 @@ class Pyomo_Problem(RealOptProblem):
         #self.upper=[None, 0.0, 2.0, -1.0]
         #self.nvars=4
     
+    
     def function_value(self, x):
-        x_here = x.get_values().values()
-        problem = self.problem
-        out     = problem.objective(x_here)  
+        x_here       = x.get_values().values()
+        problem      = self.problem
+        out          = problem.objective(x_here)  
         self.obj_val = out
         return  out
-            
+           
         
     def gradient(self, x):
-        x_here = x.get_values().values()
-        problem       = self.problem
+        x_here     = x.get_values().values()
+        problem    = self.problem
         if np.isclose(x_here, self.x_grad):
             #already computed this point
             grad_f = self.grad_f
@@ -197,28 +208,40 @@ class Pyomo_Problem(RealOptProblem):
         jac_g  = self.jac_g
         return jac_g
         
-    def  nonlinear_constraint_values(self,x):
+    def nonlinear_constraint_values(self,x):
         x_here = x.get_values().values()
         print 'x_here'
         con = problem.all_constraints(x_here)
         return con
-
+    
+    def objective_rule(self, model):
+        x   = model.x
+        out = self.function_value(x)
+        return out
 
     def constraint_rule(self, model,j): #returns 0 when the constraint is violated
         #j is index for a constraint number
         #problem = model.problem
         problem = self.problem
         x       = model.x.get_values().values()    
+        #print 'x = ', x
         con     = problem.optimization_problem.constraints #problem constraint
-        con_val = problem.all_constraints(x)
-  
-        edge    = con[j][2] 
-        symb    = con[j][1]
+        con_val = problem.all_constraints(x)[j-1]  #pyomo is 1 indexed
+        edge    = con[j-1][2] 
+        symb    = con[j-1][1]
+       
         if symb == '=':
-            out =  con_val == edge   #con[j][1] is the sign
+            #out =  con_val == edge   #con[j][1] is the sign
+            out = (edge, con_val, edge)
         elif symb == '>':
-            out = con_val > edge
+            #out = con_val > edge
+            out = (edge, con_val, None)
         elif symb == '<':
-            out = con_val < edge
+            out = (None, con_val, edge)
+            #out = con_val < edge
+ 
+        #out = (con_val, symb, edge)
+
+        
         return out
             
